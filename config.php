d37393f962194751e525e48bea166c2202fc4b7d
ilearn-code <satyamyadavv931@gmail.com>
2025-07-22 01:36:54 -0300

    added ssl

--- config.php
+++ config.php
@@ -4,6 +4,9 @@ $username = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASS') ?: '';
 $dbname = getenv('DB_NAME') ?: 'test';
 $port = getenv('DB_PORT') ?: '3306';
+$ssl_ca     = __DIR__ . '/DigiCertTLSECCP384RootG5.crt.pem'; // Ensure file exists
+
+
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
