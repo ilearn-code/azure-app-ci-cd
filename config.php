824226fea99665a4a9b1e1f0b8981462ecb3b780
ilearn-code <satyamyadavv931@gmail.com>
2025-07-22 01:41:33 -0300

    updated the ssl

--- config.php
+++ config.php
@@ -4,12 +4,14 @@ $username = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASS') ?: '';
 $dbname = getenv('DB_NAME') ?: 'test';
 $port = getenv('DB_PORT') ?: '3306';
-$ssl_ca     = __DIR__ . '/DigiCertTLSECCP384RootG5.crt.pem'; // Ensure file exists
+$ssl_ca     = __DIR__ . '/DigiCertGlobalRootCA.crt.pem'; // Ensure file exists
 
 
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;port=$port;charset=utf8mb4", $username, $password, [
+        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca
+    ]);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
